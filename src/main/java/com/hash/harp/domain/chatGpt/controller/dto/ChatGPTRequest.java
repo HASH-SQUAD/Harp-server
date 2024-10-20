bfde922d56f0d2f718aadf8b2546e23ef1a8dd49
조예설 <choyeseol07@gmail.com>
2024-10-20 16:29:29 +0900

    feat: ChatGPT API 연결 테스트

---
+++ src/main/java/com/hash/harp/domain/chatGpt/controller/dto/ChatGPTRequest.java
@@ -0,0 +1,43 @@
+package com.hash.harp.domain.chatGpt.controller.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ChatGPTRequest {
+
+    private String model;
+
+    private List<Message> messages;
+
+    private Double temperature;
+
+    private int maxTokens;
+
+    private int topP;
+
+    private int frequencyPenalty;
+
+    private int presencePenalty;
+
+    public ChatGPTRequest
+            (
+                    String model, String prompt, Double temperature, int maxTokens, int topP, int frequencyPenalty, int presencePenalty
+            )
+    {
+        this.model = model;
+        this.messages = new ArrayList<>();
+        this.messages.add(new Message("user", prompt));
+        this.temperature = temperature;
+        this.maxTokens = maxTokens;
+        this.topP = topP;
+        this.frequencyPenalty = frequencyPenalty;
+        this.presencePenalty = presencePenalty;
+
+    }
+}

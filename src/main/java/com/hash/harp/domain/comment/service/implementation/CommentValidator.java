134f03780ecf80b3ebdebf0d4cdd28e5fd0f62dd
조예설 <choyeseol07@gmail.com>
2024-10-29 14:24:05 +0900

    refactor: Validator 파일명 수정...

--- src/main/java/com/hash/harp/domain/comment/service/implementation/CommentValidator.java
+++ src/main/java/com/hash/harp/domain/comment/service/implementation/CommentValidator.java
@@ -5,7 +5,7 @@ import com.hash.harp.domain.user.domain.User;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CommentValidater {
+public class CommentValidator {
     public void checkCommentAuthor(User commentWriter, User writer) {
         if (!commentWriter.getId().equals(writer.getId())) {
             throw new CommentNotAuthorException();

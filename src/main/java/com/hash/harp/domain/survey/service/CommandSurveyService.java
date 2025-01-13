7b012eef21113208dc4de95dced15bebe602ff53
조예설 <choyeseol07@gmail.com>
2025-01-13 15:45:08 +0900

    fix: SurveyCreator 분리

--- src/main/java/com/hash/harp/domain/survey/service/CommandSurveyService.java
+++ src/main/java/com/hash/harp/domain/survey/service/CommandSurveyService.java
@@ -1,39 +1,17 @@
 package com.hash.harp.domain.survey.service;
 
 import com.hash.harp.domain.survey.controller.dto.SurveyRequestDto;
-import com.hash.harp.domain.survey.domain.Survey;
-import com.hash.harp.domain.survey.repository.SurveyRepository;
-import com.hash.harp.domain.user.domain.User;
-import com.hash.harp.domain.user.exception.UserNotFoundException;
-import com.hash.harp.domain.user.repository.UserRepository;
+import com.hash.harp.domain.survey.service.implementation.SurveyCreator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommandSurveyService {
 
-    private final SurveyRepository surveyRepository;
-    private final UserRepository userRepository;
+    private final SurveyCreator surveyCreator;
 
     public void createSurvey(SurveyRequestDto surveyRequestDto, Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException(userId));
-
-        Survey survey = Survey.builder()
-                .userId(user.getId())
-                .travel(surveyRequestDto.getTravelAsEnum())
-                .food(surveyRequestDto.getFoodAsEnum())
-                .mbti(surveyRequestDto.mbti())
-                .content(surveyRequestDto.content())
-                .build();
-
-        surveyRepository.save(survey);
-
-        if (user.getIsFirst()) {
-            user.update();
-            userRepository.save(user);
-        }
+        surveyCreator.createSurvey(surveyRequestDto, userId);
     }
 }
